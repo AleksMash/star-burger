37e13ef58025ebc040c01558615a0b1c6463423d
AleksMash <amashukov@yandex.ru>
2023-08-06 18:14:26 +0600

    Remove commented code

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -42,7 +42,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 RB_TOKEN = env.str('RB_TOKEN', None)
